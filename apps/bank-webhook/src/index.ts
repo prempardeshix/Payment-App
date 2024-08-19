30fd75e499f7f41f22992363ccf22e7656dafbe3
Prem Pardeshi <99177485+premrpardeshi@users.noreply.github.com>
2024-08-20 00:31:03 +0530

    Update index.ts

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -6,9 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.post("/hdfcWebhook", async (req, res) => {
-  //TODO: Add zod validation here?
-  //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
-
+  
   const paymentInformation: {
     token: string;
     userId: string;
@@ -45,7 +43,6 @@ app.post("/hdfcWebhook", async (req, res) => {
         },
         data: {
           amount: {
-            // You can also get this from your DB
             increment: Number(paymentInformation.amount),
           },
         },
